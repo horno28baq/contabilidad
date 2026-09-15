e91ec56af845de500a3eff15be0c9e44158705ef
horno28baq <horno28baq@gmail.com>
2026-09-14 19:00:25 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@ window.HORNO28_SUPABASE_URL =
 
 // Supabase Publishable Key
 window.HORNO28_SUPABASE_PUBLISHABLE_KEY =
-    'sb_publishable_eOFVdJGI9';
+    'sb_publishable_eOFVdJGI9gMfi8wXorHpuw_7BWvKDxy';
 
 // Configuration status
 window.HORNO28_SUPABASE_CONFIGURED =
